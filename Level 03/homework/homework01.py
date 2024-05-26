031cc241e5d88ce66648519a8e6d41e2bad5dfcb
mixeildevidze <devidzemixeil43@gmail.com>
2024-05-26 03:05:35 -0700

    commit

---
+++ Level 03/homework/homework01.py
@@ -0,0 +1,13 @@
+age = 16
+name = "Misho"
+surname = "Devidze"
+height = 1.80
+GOA_level = 3
+determination = True
+living_planet = "Earth"
+living_continent = "Europe"
+living_country = "Georgia"
+living_city = "Borjomi"
+print("My name is" + " " + name)
+print("My surname is" + " " + surname)
+print("I live in" + "," + living_planet + " " + living_continent + " " + living_country + " " + living_city)
