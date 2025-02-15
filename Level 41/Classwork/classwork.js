403b2da1675e793569a837718ce25367b73755a7
mixeildevidze <devidzemixeil43@gmail.com>
2025-02-15 03:22:30 -0800

    commit

---
+++ Level 41/Classwork/classwork.js
@@ -0,0 +1,28 @@
+function helloWorld() {
+    console.log("Hello world");
+}
+
+helloWorld();
+
+
+
+function sumNums(a,b,c) {
+    console.log(a + b + c);
+
+}
+sumNums(5, 10, 20)
+
+
+
+function sum(a = 0, b = 0) {
+    return a + b;
+}
+console.log(sum(5, 10))
+
+
+
+
+// ფუნქცია python-ში იწერება def-ით ხოლო js-ში function-ით ასევე python-ში მნიშვნელოვანია დავიცვათ ინდენტაცია ხოლო js-ში არაა სავალდებულო და კიდევ js-ში ვიყენებთ წერტილ-მძმეს ლაინის ბოლოს
+
+
+// DOM არის Document Object Model რომელიც ძალიან მნიშვნელოვანია საიტის სწორად ფუნქციონირებისთვის იგი წარმოგვიდგენს html დოკუმენტს როგორც ხეს რაც საშუალებას გვაძლევს, დინამიურად შევცვალოთ, დავამატოთ ან წავშალოთ HTML ელემენტები JavaScript-ის საშუალებით.
